b13158c06896100fa8b0d81d7e600a4056f62378
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-20 11:13:20 -0300

    feat: Creating Categorias Resources file

---
+++ src/main/java/com/ian/petshop/Resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.ian.petshop.Resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String mostrar() {
+    return "Categorias";
+  }
+}
