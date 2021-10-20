3ac6b3fe0c83a333930b4184bb5387dbdb9cb3d5
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-20 14:42:33 -0300

    feat: Creating Domain Class Categoria and Change Categoria Resource

--- src/main/java/com/ian/petshop/Resources/CategoriaResource.java
+++ src/main/java/com/ian/petshop/Resources/CategoriaResource.java
@@ -1,15 +1,30 @@
-package com.ian.petshop.Resources;
+package com.ian.petshop.resources;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ian.petshop.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
   @RequestMapping(method = RequestMethod.GET)
-  public String mostrar() {
-    return "Categorias";
+  public List<Categoria> mostrar() {
+    Categoria cat1 = new Categoria(1,"Alimento");
+    Categoria cat2 = new Categoria(2,"Cosmético");
+    Categoria cat3 = new Categoria(3,"Remédio");
+
+    ArrayList<Categoria> lista = new ArrayList<>();
+
+    lista.add(cat1);
+    lista.add(cat2);
+    lista.add(cat3);
+
+    return lista;
   }
 }
