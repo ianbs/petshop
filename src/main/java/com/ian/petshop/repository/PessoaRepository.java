4349f658b1f4117b900692f6aeb4151885f415b0
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-26 10:32:52 -0300

    feat: Creating PessoaRepository

---
+++ src/main/java/com/ian/petshop/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.ian.petshop.repository;
+
+import com.ian.petshop.domain.Pessoa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+}
