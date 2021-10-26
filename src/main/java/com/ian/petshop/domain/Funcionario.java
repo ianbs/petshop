a834cfa8aad4583bc1dedc7618f387cc54567be1
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-26 09:26:09 -0300

    feat: Creating child class Funcionario

---
+++ src/main/java/com/ian/petshop/domain/Funcionario.java
@@ -0,0 +1,23 @@
+package com.ian.petshop.domain;
+
+public class Funcionario extends Pessoa {
+  public static final long serialVersionUID = 1L;
+
+  private String funcao;
+
+  public Funcionario() {
+  }
+
+  public Funcionario(Integer id, String nome, String email, String codNacional, String funcao) {
+    super(id, nome, email, codNacional);
+    this.setFuncao(funcao);
+  }
+
+  public String getFuncao() {
+    return funcao;
+  }
+
+  public void setFuncao(String funcao) {
+    this.funcao = funcao;
+  }
+}
