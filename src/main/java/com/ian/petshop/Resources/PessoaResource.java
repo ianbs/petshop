f5395bce0ee6ddbaf4f9fcbe65f33bf9991c0660
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-28 09:45:47 -0300

    feat: Creating CRUD operations for Pessoa

--- src/main/java/com/ian/petshop/Resources/PessoaResource.java
+++ src/main/java/com/ian/petshop/Resources/PessoaResource.java
@@ -3,9 +3,13 @@ package com.ian.petshop.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 import com.ian.petshop.domain.Pessoa;
 import com.ian.petshop.service.PessoaService;
@@ -18,8 +22,29 @@ public class PessoaResource {
   PessoaService service;
 
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public ResponseEntity<Pessoa> findCategorias(@PathVariable Integer id) {
+  public ResponseEntity<Pessoa> findPessoas(@PathVariable Integer id) {
     Pessoa objPessoa = service.findPessoa(id);
     return ResponseEntity.ok().body(objPessoa);
   }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Pessoa> insertPessoas(@RequestBody Pessoa pessoa) {
+    Pessoa objPessoa = service.insertPessoa(pessoa);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objPessoa.getId()).toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Void> updatePessoas(@RequestBody Pessoa pessoa, @PathVariable Integer id) {
+    pessoa.setId(id);
+    pessoa = service.updatePessoa(pessoa);
+    return ResponseEntity.noContent().build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> deletePessoas(@PathVariable Integer id) {
+    service.deletePessoa(id);
+    return ResponseEntity.noContent().build();
+  }
+
 }
--- src/main/java/com/ian/petshop/service/PessoaService.java
+++ src/main/java/com/ian/petshop/service/PessoaService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.ian.petshop.domain.Pessoa;
 import com.ian.petshop.repository.PessoaRepository;
+import com.ian.petshop.service.exceptions.DataIntegrityException;
 import com.ian.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +21,25 @@ public class PessoaService {
     Optional<Pessoa> obj = repository.findById(id);
     return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(
         "Objeto não encontrado. ID: " + id + ", Tipo: " + Pessoa.class.getName()));
+  }
+
+  public Pessoa insertPessoa(Pessoa pessoa) {
+    pessoa.setId(null);
+    return repository.save(pessoa);
+  }
+
+  public Pessoa updatePessoa(Pessoa pessoa) {
+    findPessoa(pessoa.getId());
+    return repository.save(pessoa);
+  }
+
+  public void deletePessoa(Integer id) {
+    findPessoa(id);
+    try {
+      repository.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DataIntegrityException("Não é possivel excluir.");
+    }
   }
 
 }
