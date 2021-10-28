e1ae6b681060dd0129f31611fd4bc29ade06196c
Ian B. S <ianbarbosasantos@gmail.com>
2021-10-28 10:08:00 -0300

    feat: Creating CRUD operations for Servico

--- src/main/java/com/ian/petshop/Resources/ServicoResource.java
+++ src/main/java/com/ian/petshop/Resources/ServicoResource.java
@@ -1,14 +1,18 @@
 package com.ian.petshop.resources;
 
+import java.net.URI;
+
 import com.ian.petshop.domain.Servico;
 import com.ian.petshop.service.ServicoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/servicos")
@@ -23,4 +27,23 @@ public class ServicoResource {
     return ResponseEntity.ok().body(objServico);
   }
 
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Servico> insertServicos(@RequestBody Servico servico) {
+    Servico objServico = service.insertServico(servico);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objServico.getId()).toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Void> updateServicos(@RequestBody Servico servico, @PathVariable Integer id) {
+    servico.setId(id);
+    servico = service.updateServico(servico);
+    return ResponseEntity.noContent().build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> deleteServicos(@PathVariable Integer id) {
+    service.deleteServico(id);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/com/ian/petshop/service/ServicoService.java
+++ src/main/java/com/ian/petshop/service/ServicoService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.ian.petshop.domain.Servico;
 import com.ian.petshop.repository.ServicoRepository;
+import com.ian.petshop.service.exceptions.DataIntegrityException;
 import com.ian.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +20,25 @@ public class ServicoService {
     Optional<Servico> obj = servicoRepository.findById(id);
     return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(
         "Objeto não encontrado. ID: " + id + ", Tipo: " + Servico.class.getName()));
+  }
+
+  public Servico insertServico(Servico servico) {
+    servico.setId(null);
+    return servicoRepository.save(servico);
+  }
+
+  public Servico updateServico(Servico servico) {
+    findServico(servico.getId());
+    return servicoRepository.save(servico);
+  }
+
+  public void deleteServico(Integer id) {
+    findServico(id);
+    try {
+      servicoRepository.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DataIntegrityException("Servico possui produtos. Não é possivel excluir.");
+    }
   }
 
 }
